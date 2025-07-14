730c0a54d74d44e3952965a9a9158d3d63e67400
Sumanth <sumanthpoojary965@gmail.com>
2025-07-14 14:29:50 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './Pages/Register';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import './index.css';
 import Login from './Pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const clientId = "854312165167-ai2tevkg711312s6tjs3ujsv380nc3pc.apps.googleusercontent.com";
 
@@ -15,6 +15,9 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Register />} />
                     <Route path="/login" element={<Login />} />
+                    
+                    {/* Redirect all unknown routes to Register */}
+                    <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
             </Router>
         </GoogleOAuthProvider>
